dc685e72bef19bfbe8024c4c4d82fca67f2e17d1
Janos Gyerik <janos@kronos>
2018-01-28 13:37:12 +0100

    Drop the sidebar and tabs

--- game-stats/ui.R
+++ game-stats/ui.R
@@ -1,19 +1,9 @@
 library(shiny)
 
 source('common.R')
-source('about.R')
 
 ui <- fluidPage(
   titlePanel("Game Stats"),
   
-  sidebarLayout(
-    sidebarPanel(),
-    mainPanel(
-      tabsetPanel(
-        type = "tabs", selected = "Stats",
-        #tabPanel("About", about),
-        tabPanel("Stats", dataTableOutput("stats"))
-      )
-    )
-  )
+  dataTableOutput("stats")
 )
