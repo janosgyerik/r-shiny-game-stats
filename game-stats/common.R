b48618fffca56abd489cd6cc0db832de3a489ce5
Janos Gyerik <janos@kronos>
2018-01-28 14:39:06 +0100

    Add won column

--- game-stats/common.R
+++ game-stats/common.R
@@ -1,6 +1,6 @@
-stats.df <- data.frame(
-  game=c(1, 1, 2, 2),
-  player=c("Janos", "Vika", "Janos", "Vika"),
-  corp=c("Helion", "Ecoline", "Teractor", "Credicor"),
-  vp_total=c(34, 41, 21, 23)
-)
+stats.df <- read.csv("data/stats.csv")
+
+wins <- aggregate(vp_total ~ game, data = stats.df, max)
+stats.df$won <- sapply(1:nrow(stats.df), function(i) {
+  wins$vp_total[wins$game == stats.df$game[i]] == stats.df$vp_total[i]
+})
